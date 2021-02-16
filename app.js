ae3e438bfa6ef589580413a5b663e413ca2b90cf
YeeHeng <lauyh87@gmail.com>
2021-02-16 23:56:35 +0800

    Update route & serve html page

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.listen(8888);
+// error handler
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(8888, ()=>{
+    console.log(`Server is running on port 8888`)
+});
