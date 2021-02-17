8a5140bde83e0959d539ff89dca13a797e5a20dc
YeeHeng <lauyh87@gmail.com>
2021-02-17 23:44:34 +0800

    remove 404 page

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 // error handler
-app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+// app.use((req, res, next)=>{
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// })
 
 app.listen(8888, ()=>{
     console.log(`Server is running on port 8888`)
